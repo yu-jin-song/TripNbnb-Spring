7ce53092c790aac03ab13bc1c5729a4bdcd38ffb
yu-jin-song <yujinsong.tech@gmail.com>
2023-04-18 21:29:10 +0900

    Update .gitignore

---
+++ src/main/java/com/joy/tripnbnb/TripnbnbApplication.java
@@ -0,0 +1,13 @@
+package com.joy.tripnbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripnbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripnbnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joy/tripnbnb/TripnbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joy.tripnbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripnbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
