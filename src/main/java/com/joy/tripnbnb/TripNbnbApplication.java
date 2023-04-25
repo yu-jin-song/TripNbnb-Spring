5b2289d71b7fcbbc7b65458275dce271f824db74
yu-jin-song <yujinsong.tech@gmail.com>
2023-04-25 18:53:20 +0900

    Rename application name

--- src/main/java/com/joy/tripnbnb/TripNbnbApplication.java
+++ src/main/java/com/joy/tripnbnb/TripNbnbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripnbnbApplication {
+public class TripNbnbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TripnbnbApplication.class, args);
+		SpringApplication.run(TripNbnbApplication.class, args);
 	}
 
 }
